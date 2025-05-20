7d731942a0902405fd0e7c231b700df1ee8ad29c
Juandvr <villabonajuan203@gmail.com>
2025-05-20 01:03:57 -0500

    se agrego la ruta para el modulo de cuadre de maquina

--- backend/app/modules/main.py
+++ backend/app/modules/main.py
@@ -4,6 +4,7 @@ from app.modules.usuarios_configuracion import usuarios
 from app.modules.usuarios_configuracion import acciones_usuario
 from app.modules.gestion_maquinas import maquinas
 from app.modules.gestion_maquinas import acciones_maquina
+from app.modules.cuadre_maquina import cuadre_maquina
 
 
 app = FastAPI()
@@ -11,6 +12,7 @@ app.include_router(usuarios.router)
 app.include_router(acciones_usuario.router)
 app.include_router(maquinas.router)
 app.include_router(acciones_maquina.router)
+app.include_router(cuadre_maquina.router)
 
 
 app.add_middleware(
